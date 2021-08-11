2c857b090ce92099f0aefd6c2f457b67ab8e7751
ParseSnip <sean.m.devoy@gmail.com>
2021-08-11 11:42:41 -0400

    added unit testing workflow

---
+++ test/test.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+const expect = require('chai').expect
+//const require = require('chai').should()
+
+
+describe('testing a unit test', function(){
+    it('should return true', function(){
+        assert.equal(3,3)
+    })
+})
